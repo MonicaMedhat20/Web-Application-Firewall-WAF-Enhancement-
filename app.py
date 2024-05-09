cb5413da1c035897d3983d9bfc7c24bc3c6d0908
MonicaMedhat20 <Monica2000145@miuegypt.edu.eg>
2024-05-10 00:18:32 +0300

    fn

--- app.py
+++ app.py
@@ -12,18 +12,8 @@ from Final.main_code import URLChecker, predict_from_url
 #creating instance of the app
 app = Flask(__name__, static_folder='./static')
 
-# model = pickle.load(open('model.pkl', '+rb')) #model generated for dataset1
-# model2 = pickle.load(open('model2.pkl', '+rb')) #model generated for dataset2
 
 
-
-
-#loading the pre-trained machine learning model
-# with open('model.pkl','rb') as model_file:
-#     RF_model = pickle.load(model_file)
-
-# with open('model2.pkl','rb') as model2_file:
-#     RF_model2 = pickle.load(model2_file)
 
 #stacking of two routes to be the same route (leading to the same page) 
 @app.route('/')
@@ -85,39 +75,8 @@ if __name__ == "__main__":
 
 
 
-# def analyze_url(url):
-#     resu = get_url(url_for)
-#     return float(ml_model.predict)
-
-# @app.route('/classify', methods = ['POST'])
-# def classify_handler():
-#     payload = request.form['payload']
-#     result =get_payload(payload)  
-#     class_name = 'Malicious' if result == 1 else 'Normal'
-#     print('payload:', payload)
-#     print('result:', result)
-
-#     print('class_name:', class_name)
-
-#     return {
-#         'payload' : payload,
-#         'result' : result,
-#         'class_name': class_name
-#     } #return json 
 
 
-
-
-# @app.route('/demo.html/', methods=['GET', 'POST'])
-# def demo():
-#     if (request.method == 'POST'):
-#         payload = request.form.get('payload')
-#         return redirect(url_for('home'))
-#     return render_template('demo.html')
-
-# @app.route('/demo.html/home.html/')
-# def home2():
-#     return render_template('home.html')
 
 
 
